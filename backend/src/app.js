b8d77e7028f3e79cb391724829f9c4eca693b889
Md Ashif Iqbal <contact2ashif@gmail.com>
2026-06-28 22:37:52 +0530

    project init

---
+++ backend/src/app.js
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import mentorRouter from "./routes/mentor.route.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/v1/mentor", mentorRouter);
+app.use(errorMiddleware);
+export default app;
